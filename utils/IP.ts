febb42fe0ac9596732de161bc5a9acbeb58d7c76
Yasir Ali <yasiraliraj@gmail.com>
2026-08-08 22:17:45 +0500

    Update IP

--- utils/IP.ts
+++ utils/IP.ts
@@ -1,4 +1,4 @@
-const LOCAL_IP = "http://192.168.18.22:5000";
+const LOCAL_IP = "http://192.168.1.9:5000";
 const LIVE_URL = "https://your-live-api-url.com";
 
 export const IP = __DEV__ ? LOCAL_IP : LIVE_URL;
